517c4476b525bf21b49c284af7d488dac48ef45c
uncomm1t <atschalow@yahoo.de>
2024-04-25 15:31:48 +0200

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,10 @@
+
+import "bootstrap/dist/css/bootstrap.min.css";
+//import './assets/main.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+import "bootstrap/dist/js/bootstrap.min.js"
+
+
+
+createApp(App).mount('#app')
