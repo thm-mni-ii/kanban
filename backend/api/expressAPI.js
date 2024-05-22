3d115adfc557f6d1fb8e34f07c9d3d3adf35c850
uncomm1t <atschalow@yahoo.de>
2024-05-22 16:09:04 +0200

    added cors to the expressAPI.js

--- backend/api/expressAPI.js
+++ backend/api/expressAPI.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require('cors');
 
+app.use(cors());
 //  TODO: remove Hello World
 app.get("/", (req, res) => {
   res.send("Hello World!");
