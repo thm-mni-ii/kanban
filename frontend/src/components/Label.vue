abc058387cbc1fa81836d797e63666afcde7c612
uncomm1t <atschalow@yahoo.de>
2024-06-18 13:07:10 +0200

    added method to load Task into correct columns

--- frontend/src/components/Label.vue
+++ frontend/src/components/Label.vue
@@ -1,55 +1,75 @@
 <template>
-    <v-col cols="3">
-      <div class="style-box">
-        <h2>{{ sectionTitle }}</h2>
-        <draggable :list="items" group="tasks" @start="drag=true" @end="drag=false">
-          <template #item="{ element, index }">
-            <div :key="index">
-              <v-card class="mb-3">
-                <v-card-text>{{ element.title }}</v-card-text> 
-                <v-col cols="2">
-                    <v-btn icon="mdi-delete-outline" small  density="compact" @click.stop="deleteTask(index)">
-                    </v-btn>
-                  </v-col>
-              </v-card>
-            </div>
-          </template>
-        </draggable>
-      </div>
-    </v-col>
-  </template>
-  
-  <script>
-  import { ref } from 'vue';
-  import draggable from 'vuedraggable';
-  
-  export default {
-    components: {
-      draggable
-    },
-    props: {
-      sectionTitle: String,
-      items: Array
-    },
-    setup(props, { emit }) {
-      const deleteTask = (index) => {
-        props.items.splice(index, 1);
-        emit('update:items', props.items);
-      }
-  
-      return {
-        deleteTask
-      }
+  <v-col cols="3">
+    <div class="style-box">
+      <h2>{{ sectionTitle }}</h2>
+      <draggable :list="cards" group="tasks" @start="drag = true" @end="drag = false">
+        <template #item="{ element, index }">
+          <div :key="index">
+            <v-card class="mb-3">
+              <v-card-text>{{ element.name }}</v-card-text>
+              <v-col cols="2">
+                <v-btn icon="mdi-delete-outline" small density="compact" @click.stop="deleteCard(index)">
+                </v-btn>
+              </v-col>
+            </v-card>
+          </div>
+        </template>
+      </draggable>
+    </div>
+  </v-col>
+</template>
+
+<script>
+import { ref } from 'vue';
+import draggable from 'vuedraggable';
+export default {
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      cards: [],
     }
-  }
-  </script>
+  },
+  props: {
+    sectionTitle: String,
+    items: Array,
+    status : String 
+  },
+  methods: {
+    async loadCards(){
+      try {
+        const groupId = this.$route.params.groupId;
+        const boardId = this.$route.params.boardId;
+        const response = await fetch(`http://localhost:3000/groups/${groupId}/boards/${boardId}/cards/`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        this.cards = data;
+        this.cards = this.cards.filter(card => card.status === this.status);
+        console.log('fetched data' + data + "cards: "+ this.cards);
+      } catch (error) {
+        console.error('There was an error!', error);
+      }
+    },
+    async deleteCard(index){
+      console.log(index);
+    }
+  },
   
-  <style scoped>
-  .style-box {
-    background-color: grey;
-    border: 1px solid black;  
-    padding: 20px 10px;
-    border-radius: 5px;
-    min-height: 300px;
+  created(){
+    this.loadCards();
   }
-  </style>
+}
+</script>
+
+<style scoped>
+.style-box {
+  background-color: grey;
+  border: 1px solid black;
+  padding: 20px 10px;
+  border-radius: 5px;
+  min-height: 300px;
+}
+</style>
