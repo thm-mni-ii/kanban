dcd0c0a75d8ca5ea55436bd4b67cee352372f36d
MehmetDenizGültekin <mehmet.deniz.gueltekin@mnd.thm.de>
2024-09-24 19:15:06 +0200

    add BarChart Component & example Data in Start.vue

--- frontend/src/Start.vue
+++ frontend/src/Start.vue
@@ -1,69 +1,90 @@
 <template>
-    <v-app>
-        <v-container v-if="isStart">
-            <v-card v-for="(item, index) in items" :key="index"  class="mb-4"
-                :style="{ backgroundColor: getColorFromId(item.group) }" @click="onGroupCardClick(item.group)">
-                <v-card-title>
-                    GruppeID: {{ item.group }}
-                </v-card-title>
-            </v-card>
-            <div v-if="groupSelected">
-                Ausgewählte Gruppe: {{ groupSelected }}
-                <ul>
-                    <li v-for="member in selectedGroupMembers" :key="member.id">
-                        {{ member.first_name }} {{ member.last_name }}
-                    </li>
-                </ul>
-            </div>
-        </v-container>
-        <router-view v-if="!isStart"></router-view>
-    </v-app>
+  <v-app>
+    <v-container v-if="isStart">
+      <v-card
+        v-for="(item, index) in items"
+        :key="index"
+        class="mb-4"
+        :style="{ backgroundColor: getColorFromId(item.group) }"
+        @click="onGroupCardClick(item.group)"
+      >
+        <v-card-title> GruppeID: {{ item.group }} </v-card-title>
+      </v-card>
+      <div v-if="groupSelected">
+        Ausgewählte Gruppe: {{ groupSelected }}
+        <ul>
+          <li v-for="member in selectedGroupMembers" :key="member.id">
+            {{ member.first_name }} {{ member.last_name }}
+          </li>
+        </ul>
+      </div>
+      <!--Beispieldatensatz für den BarChart-->
+      <BarChart :data="{ labels: [ 'Red', 'Blue', 'Yellow', 'Green', 'Purple',
+      'Orange', 'FakeFarbe', 'FakeFarbe', ], datasets: [ { label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3, 100, 1], borderWidth: 1, }, ], }"
+      />
+    </v-container>
+    <router-view v-if="!isStart"></router-view>
+  </v-app>
 </template>
 
 <script>
+import { getColorFromId } from "./colorHelper";
+import BarChart from "./components/BarChart.vue";
 
-import { getColorFromId } from './colorHelper';
+// Ich war nicht in der Lage herauszufinden, was man machen muss, damit man die Variable im Template benutzen kann
+const options = {
+  scales: {
+    y: {
+      beginAtZero: true,
+    },
+  },
+};
 
 export default {
-    data() {
-        return {
-            items: [],
-            mockData: [],
-            groupSelected: null,
-            selectedGroupMembers: []
-        };
-    },
-    computed: {
-        isStart() {
-            return this.$route.path === '/';
-        }
-    },
-    created() {
-        fetch('/groups.json')
-            .then(response => response.json())
-            .then(data => {
-                this.items = data;
-            });
+  components: { BarChart },
 
-        fetch('/MOCK_DATA.json')
-            .then(response => response.json())
-            .then(data => {
-                this.mockData = data;
-            });
+  data() {
+    return {
+      items: [],
+      mockData: [],
+      groupSelected: null,
+      selectedGroupMembers: [],
+    };
+  },
+  computed: {
+    isStart() {
+      return this.$route.path === "/";
     },
-    methods: {
-        selectGroup(groupId) {
-            this.groupSelected = groupId;
-            const group = this.items.find(item => item.group === groupId);
-            this.selectedGroupMembers = this.mockData.filter(member => group.members.includes(member.id));
-        },
-        getColorFromId,
+  },
+  created() {
+    fetch("/groups.json")
+      .then((response) => response.json())
+      .then((data) => {
+        this.items = data;
+      });
 
-        onGroupCardClick(groupId) {
-            this.$router.push({ name: 'Boards', params: { groupId: groupId } });
-        }
+    fetch("/MOCK_DATA.json")
+      .then((response) => response.json())
+      .then((data) => {
+        this.mockData = data;
+      });
+  },
+  methods: {
+    selectGroup(groupId) {
+      this.groupSelected = groupId;
+      const group = this.items.find((item) => item.group === groupId);
+      this.selectedGroupMembers = this.mockData.filter((member) =>
+        group.members.includes(member.id)
+      );
     },
-}
+    getColorFromId,
+
+    onGroupCardClick(groupId) {
+      this.$router.push({ name: "Boards", params: { groupId: groupId } });
+    },
+  },
+};
 </script>
 
 <style scoped></style>
